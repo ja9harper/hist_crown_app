c7fce3960a8b873a52cd769adae64a6bc152f2f6
ja9harper <ja9harper@gmail.com>
2014-10-29 10:57:17 -0400

    added user auth stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,37 @@
 class UsersController < ApplicationController
+  
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :load_user
+  #, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, :authorize
+  def index
+    @users = User.all
+  end
 
+  # GET /users/1
+  def show
+    if !load_user
+      redirect_to root_path
+    elsif logged_in?
+      redirect_to login_path
+    else
+      render(:show)
+    end
+  end
+    #make sure the user is autheticated
+
+    #make sure authenticated user is the user
+
+    #who is trying o access this? are they a user
+  #   current_sessions_user = User.find_by(id: session[:user_id])
+  #   if !current_sessions_user.present?
+  #     redirect_to login_path
+  #   elsif current_sessions_user != @user
+  #     redirect_to user_path(current_sessions_user)
+  #   else
+  #     render(:show)
+  # end
+end
   # GET /users
   # GET /users.json
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :email, uniqueness: true
+
+  has_secure_password
 end
